3a8bbda4494f8038c5f570e31319faf4486f0e80
GustavoBento <gustavo_grb-@hotmail.com>
2024-09-24 20:21:07 -0300

    migrando pro ts

--- src/components/Banner/index.jsx
+++
@@ -1,9 +0,0 @@
-import './Banner.css'
-
-export default function Banner() {
-    return (
-        <header className='banner'>
-            <img src="./imagens/banner.png" alt="Imagem do banner principal." />
-        </header>
-    )
-}
